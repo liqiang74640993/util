8f86f39ff6763c6de1c0aaba4f0f0e324a1a879e
liqiang74640993 <liqiang74640993@gmail.com>
2025-05-19 11:24:26 +0800

    修改main.cpp

--- api_util/test/main.cpp
+++ api_util/test/main.cpp
@@ -1,6 +1,11 @@
 #include <iostream>
 #include <gtest/gtest.h>
 
+int temp(char *s){
+    printf("%s\n",s);
+    return 0;
+}
+
 int main(int argc, char **argv) {
     ::testing::InitGoogleTest(&argc, argv);
     ::testing::GTEST_FLAG(filter) = "ProcessIPCTest.*";
